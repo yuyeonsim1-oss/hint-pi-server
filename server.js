f0a8a69238e5f0eeab2178ea2c9ad48163d452db
yuyeonsim1-oss <yuyeonsim1@gmail.com>
2026-04-07 21:41:02 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.use(express.json());
+
+const PI_API_KEY = process.env.PI_API_KEY;
+
+app.get("/", (req, res) => {
+  res.send("Pi payment server running");
+});
+
+app.post("/approve", async (req, res) => {
+  const { paymentId } = req.body;
+
+  try {
+    await axios.post(
+      `https://api.minepi.com/v2/payments/${paymentId}/approve`,
+      {},
+      {
+        headers: {
+          Authorization: `Key ${PI_API_KEY}`
+        }
+      }
+    );
+
+    res.send({ success: true });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: "approve failed" });
+  }
+});
+
+app.listen(3000, () => console.log("server start"));
